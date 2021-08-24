f5776233cc068a82510d18df6bc2186e88041682
edumueller <eduardo.nicastro@icloud.com>
2021-08-23 22:00:50 -0300

    feat: add initial listener logic and receive msg

--- src/listener.ts
+++ src/listener.ts
@@ -7,5 +7,11 @@ const stan = nats.connect("ticketing", "123", {
 });
 
 stan.on("connect", () => {
-  console.log("Listener connected to nats");
+  console.log("Listener connected to NATS");
+
+  const subscription = stan.subscribe("ticket:created");
+
+  subscription.on("message", (msg) => {
+    console.log("Message received");
+  });
 });
