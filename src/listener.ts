62ace02a46e577be25aba977b8dead9b89832e50
edumueller <eduardo.nicastro@icloud.com>
2021-08-25 20:14:56 -0300

    feat: deliver missed events

--- src/listener.ts
+++ src/listener.ts
@@ -15,12 +15,13 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("accounting-service");
+
+  const subscription = stan.subscribe("ticket:created", options);
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
